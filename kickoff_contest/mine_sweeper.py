53a4e5a66143a2a9ee8539e0287abb195ce63667
Huy Le Nhat <73874589+uitnhathuy@users.noreply.github.com>
2021-05-15 23:07:47 +0700

    Update mine_sweeper.py

--- kickoff_contest/mine_sweeper.py
+++ kickoff_contest/mine_sweeper.py
@@ -2,14 +2,8 @@ import math
 
 n, m = map(int, input().split())
 deg = [[int(x) for x in input().split()] for i in range(n)]
-b = [[-1 for j in range(m)] for i in range(n)]
+b = [[0 for j in range(m)] for i in range(n)]
 ########
-
-def set_neighbor(i, j, x):
-	if i > 0: b[i-1][j] = x
-	if i < n-1: b[i+1][j] = x
-	if j > 0: b[i][j-1] = x
-	if j < m-1: b[i][j+1] = x
 
 def rest_deg(i, j):
 	ans = 0
@@ -19,49 +13,43 @@ def rest_deg(i, j):
 	if (j < m-1) and (b[i][j+1] == 1): ans+=1
 	return deg[i][j] - ans
 
+def get_result():
+	if rest_deg(n-1, m-1) !=0:
+		return
+	for i in range(n):
+		for j in range(m):
+			print(b[i][j], end=' ')
+		print()
+	quit()
+
 def calc(i, j):
-	if j == 0 and i > 1:
-		for y in range(m):
-			if rest_deg(i-2, y) != 0:
-				return
+	for x in range(2):
+		b[i][j] = x
+		stop = False
 
-	if i >= n:
-		stop = True
-		for x in range(n):
-			for y in range(m):
-				if rest_deg(x, y) != 0: 
-					stop = False
-					break
-			if not stop:
+		if i > 0 and rest_deg(i-1, j) != 0:
+			continue
+
+		for k in range(1, min(n-i, j+1)):
+			temp = rest_deg(i+k-1, j-k)
+			if temp!=0 and temp!=1:
+				stop = True
 				break
-		if stop:
-			for x in range(n):
-				for y in range(m):
-					print(b[x][y], end=' ')
-				print()
-			quit()
-		return
+			b[i+k][j-k] = temp
 
-	x, y = i + (j+1)//m, (j+1) % m
+		if n-i < j+1 and rest_deg(n-1, j-n+i) != 0:
+			stop = True
 
-	if b[i][j] != -1:
-		calc(x, y)
-		return
-	if (i == 0) or (rest_deg(i-1, j) == 0):
-		b[i][j] = 0
-		calc(x, y)
-	if (i == 0) or (rest_deg(i-1, j) == 1):
-		b[i][j] = 1
-		calc(x, y)
-	b[i][j] = -1
+		if not stop:
+			if i == n-1 and  j == m-1:
+				get_result()
+			elif i+j+1 < m:
+				calc(i, j+1)
+			else:
+				calc(i+1, j)
+
+		for k in range(1, min(n-i, j+1)):
+			b[i+k][j-k] = 0
 
 ########
-
-for i in range(n):
-	for j in range(m):
-		if deg[i][j] == 0:
-			set_neighbor(i, j, 0)
-		elif deg[i][j] == 4:
-			set_neighbor(i, j, 1)
-
 calc(0, 0)
